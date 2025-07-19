5105d6210a37dacd1abb52f19eb66021faf89025
Harish P S <99491451+psharishgithub@users.noreply.github.com>
2025-07-19 17:23:39 +0530

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -53,6 +53,7 @@ export default buildConfig({
     'http://localhost:4200',
     'https://reccms.flashserver.in',
     'https://cmsrec.flashserver.in',
+    'http://192.250.228.70'
     process.env.FRONTEND_URL || '',
   ],
   csrf: [
@@ -62,6 +63,7 @@ export default buildConfig({
     'http://localhost:4200',
     'https://reccms.flashserver.in',
     'https://cmsrec.flashserver.in',
+    'http://192.250.228.70:3000'
     process.env.FRONTEND_URL || '',
   ],
   sharp,
